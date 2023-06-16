0e34d398f4fb2809ca981e238890388b975d4dbb
Devansh <devansh.vats.qodic@gmail.com>
2023-06-16 16:40:21 +0530

    MUI

---
+++ src/pages/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Menu = () => {
+  return (
+    <Layout>Menu Page</Layout>
+  )
+}
+
+export default Menu
---
+++ src/pages/Pagenotfound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const Pagenotfound = () => {
+  return (
+    <Layout>Page Not Found</Layout>
+  )
+}
+
+export default Pagenotfound
